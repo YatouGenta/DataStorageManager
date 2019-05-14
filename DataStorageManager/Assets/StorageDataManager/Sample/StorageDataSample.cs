0fa050e3c4d3f4625048fc69cb8831a46e62d3a2
YatouGenta <yatougenta@gmail.com>
2019-05-14 15:55:48 +0900

    プロジェクト追加

---
+++ DataStorageManager/Assets/StorageDataManager/Sample/StorageDataSample.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// ストレージデータサンプル
+/// </summary>
+public class StorageDataSample : MonoBehaviour
+{
+    //結果表示用のText
+    [SerializeField]
+    private Text text = null;
+
+    /// <summary>
+    /// 保存
+    /// </summary>
+    public void OnClickStorage()
+    {
+        SampleStorageData data = new SampleStorageData();
+        data.message = "ストレージデータ送信！";
+        DataStorageManager.DataStorage(DataStorageManager.StorageKey.Sample, data);
+        text.text = "データを保存しました";
+    }
+
+    /// <summary>
+    /// 取得
+    /// </summary>
+    public void OnClickGetStorage()
+    {
+        SampleStorageData data = (SampleStorageData)DataStorageManager.GetStorageData(DataStorageManager.StorageKey.Sample);
+        if(data != null)
+        {
+            text.text = "データを取得メッセージは\n" + data.message;
+        }
+        else
+        {
+            text.text = "データを取得失敗";
+        }
+    }
+
+}
+
+public class SampleStorageData : StorageData
+{
+    public string message;
+}
---
+++ DataStorageManager/Assets/StorageDataManager/Scripts/DataStorageManager.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 受け渡し用データ預かり管理クラス
+/// </summary>
+public static class DataStorageManager
+{
+    //データ預かりキー
+    //種類が増えたらここに記述する
+    public enum StorageKey
+    {
+        Sample = 0,
+        Non = 999,
+    }
+    //データストレージディクショナリー
+    private static Dictionary<StorageKey, StorageData> storageList = new Dictionary<StorageKey, StorageData>();
+    
+    /// <summary>
+    /// データを預ける
+    /// </summary>
+    public static void DataStorage(StorageKey key, StorageData data)
+    {
+        //キーが存在しなおかつデータがあるとき
+        if (storageList.ContainsKey(key))
+        {
+            //データを保存、もし何かあったとしても強制で上書き
+            storageList[key] = data;
+        }
+        else
+        {
+            //keyがなければ追加
+            storageList.Add(key, data);
+        }
+    }
+
+    /// <summary>
+    /// 預けたデータを取得する
+    /// </summary>
+    /// <param name="key">キー</param>
+    /// <param name="getIsDelete">取得した後元のものを消すか否か,デフォルトで消す</param>
+    public static StorageData GetStorageData(StorageKey key, bool getIsDelete = true)
+    {
+        if (storageList.ContainsKey(key))
+        {
+            StorageData data = storageList[key];
+            //削除フラグが立てば消す
+            if (getIsDelete)
+            {
+                storageList.Remove(key);
+            }
+
+            return data;
+        }
+        else
+        {
+            //keyがそもそも存在しなければnull
+            return null;
+        }
+    }
+    
+}
+
+//実際にやりとりするデータクラス
+public class StorageData
+{
+    //基本継承して使用
+}
